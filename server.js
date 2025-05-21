a0a717980ea3ae3fcc5ef779dea293cf7c835b20
Questuy <gustavocostamarinho750@gmail.com>
2025-05-21 16:31:35 +0000

    fix: altera porta padrão para 8080

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.delete('/api/alunos/:id', (req, res) => {
 });
 
 // Rodar o servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
